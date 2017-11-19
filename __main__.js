703a141a8aeffbeab4beb92410858a6c39c61603
Chan-Matthew <chann.matthew@hotmail.com>
2017-11-19 06:56:11 -0500

    Add files via upload

---
+++ __main__.js
@@ -0,0 +1,646 @@
+var food = {
+  'food': {
+    'cheese': {
+      'calories': 201,
+      'fat': 25,
+      'cholesterol': 17,
+      'sodium': 12,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 25,
+      'vitaminA': 10,
+      'vitaminB': 6,
+      'vitaminC': 0,
+      'vitaminD': 3,
+      'calcium': 36,
+      'iron': 1,
+      'potassium': 0
+    },
+    'meat': {
+      'calories': 478,
+      'fat': 18,
+      'cholesterol': 81,
+      'sodium': 7,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 174,
+      'vitaminA': 0,
+      'vitaminB': 30,
+      'vitaminC': 0,
+      'vitaminD': 8,
+      'calcium': 2,
+      'iron': 21,
+      'potassium': 40
+    },
+    'chicken': {
+      'calories': 425,
+      'fat': 36,
+      'cholesterol': 52,
+      'sodium': 6,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 98,
+      'vitaminA': 5,
+      'vitaminB': 8,
+      'vitaminC': 0,
+      'vitaminD': 1,
+      'calcium': 2,
+      'iron': 12,
+      'potassium': 11
+    },
+    'beef': {
+      'calories': 213,
+      'fat': 20,
+      'cholesterol': 25,
+      'sodium': 2,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 44,
+      'vitaminA': 0,
+      'vitaminB': 36,
+      'vitaminC': 0,
+      'vitaminD': 1,
+      'calcium': 1,
+      'iron': 12,
+      'potassium': 7
+    },
+    'steak': {
+      'calories': 213,
+      'fat': 20,
+      'cholesterol': 25,
+      'sodium': 2,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 44,
+      'vitaminA': 0,
+      'vitaminB': 36,
+      'vitaminC': 0,
+      'vitaminD': 1,
+      'calcium': 1,
+      'iron': 12,
+      'potassium': 7
+    },
+    'potato': {
+      'calories': 77,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 6,
+      'sugar': 2,
+      'protein': 3,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 24,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 3,
+      'potassium': 9
+    },
+    'fries': {
+      'calories': 77,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 6,
+      'sugar': 0.6,
+      'protein': 3,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 24,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 3,
+      'potassium': 9
+    },
+    'coffee': {
+      'calories': 1,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 0,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 0,
+      'potassium': 3
+    },
+    'milk': {
+      'calories': 103,
+      'fat': 3,
+      'cholesterol': 4,
+      'sodium': 4,
+      'dietaryfiber': 0,
+      'sugar': 13,
+      'protein': 16,
+      'vitaminA': 2,
+      'vitaminB': 18,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 30,
+      'iron': 0,
+      'potassium': 16
+    },
+    'orange': {
+      'calories': 45,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 9,
+      'sugar': 9,
+      'protein': 1,
+      'vitaminA': 4,
+      'vitaminB': 0,
+      'vitaminC': 85,
+      'vitaminD': 0,
+      'calcium': 3,
+      'iron': 0,
+      'potassium': 4
+    },
+    'orangejuice': {
+      'calories': 39,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 7,
+      'protein': 1,
+      'vitaminA': 3,
+      'vitaminB': 0,
+      'vitaminC': 71,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 1,
+      'potassium': 4
+    },
+    'garlic': {
+      'calories': 0,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 0,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 0,
+      'potassium': 0
+    },
+    'pancake': {
+      'calories': 175,
+      'fat': 10,
+      'cholesterol': 15,
+      'sodium': 14,
+      'dietaryfiber': 4,
+      'sugar': 3,
+      'protein': 9,
+      'vitaminA': 3,
+      'vitaminB': 16,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 16,
+      'iron': 7,
+      'potassium': 2
+    },
+    'waffle': {
+      'calories': 175,
+      'fat': 10,
+      'cholesterol': 15,
+      'sodium': 14,
+      'dietaryfiber': 4,
+      'sugar': 3,
+      'protein': 9,
+      'vitaminA': 3,
+      'vitaminB': 16,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 16,
+      'iron': 7,
+      'potassium': 2
+    },
+    'berry': {
+      'calories': 85,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 14,
+      'sugar': 15,
+      'protein': 2,
+      'vitaminA': 1,
+      'vitaminB': 0,
+      'vitaminC': 24,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 2,
+      'potassium': 3
+    },
+    'syrup': {
+      'calories': 52,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 14,
+      'protein': 0,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 2,
+      'iron': 0,
+      'potassium': 1
+    },
+    'salad': {
+      'calories': 18,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 5,
+      'sugar': 1.9,
+      'protein': 1,
+      'vitaminA': 49,
+      'vitaminB': 0,
+      'vitaminC': 22,
+      'vitaminD': 0,
+      'calcium': 2,
+      'iron': 3,
+      'potassium': 5
+    },
+    'egg': {
+      'calories': 78,
+      'fat': 7,
+      'cholesterol': 62,
+      'sodium': 2,
+      'dietaryfiber': 0,
+      'sugar': 0.6,
+      'protein': 12,
+      'vitaminA': 5,
+      'vitaminB': 10,
+      'vitaminC': 0,
+      'vitaminD': 11,
+      'calcium': 2,
+      'iron': 3,
+      'potassium': 1
+    },
+    'hard boiled egg': {
+      'calories': 78,
+      'fat': 7,
+      'cholesterol': 62,
+      'sodium': 2,
+      'dietaryfiber': 0,
+      'sugar': 0.6,
+      'protein': 12,
+      'vitaminA': 5,
+      'vitaminB': 10,
+      'vitaminC': 0,
+      'vitaminD': 11,
+      'calcium': 2,
+      'iron': 3,
+      'potassium': 1
+    },
+    'syrup': {
+      'calories': 52,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 14,
+      'protein': 0,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 2,
+      'iron': 0,
+      'potassium': 1
+    },
+    'bread': {
+      'calories': 69,
+      'fat': 1,
+      'cholesterol': 0,
+      'sodium': 4,
+      'dietaryfiber': 7,
+      'sugar': 1.6,
+      'protein': 7,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 3,
+      'iron': 3,
+      'potassium': 1
+    },
+    'tomato': {
+      'calories': 16,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 2.4,
+      'protein': 1,
+      'vitaminA': 15,
+      'vitaminB': 0,
+      'vitaminC': 20,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 1,
+      'potassium': 6
+    },
+    'doughnut': {
+      'calories': 195,
+      'fat': 11,
+      'cholesterol': 2,
+      'sodium': 5,
+      'dietaryfiber': 3,
+      'sugar': 1,
+      'protein': 4,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 1,
+      'vitaminD': 0,
+      'calcium': 1,
+      'iron': 9,
+      'potassium': 2
+    },
+    'hamburger': {
+      'calories': 354,
+      'fat': 26,
+      'cholesterol': 18,
+      'sodium': 20,
+      'dietaryfiber': 4,
+      'sugar': 5,
+      'protein': 40,
+      'vitaminA': 0,
+      'vitaminB': 28,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 1,
+      'iron': 19,
+      'potassium': 7
+    }
+  }
+};
+var meal = {
+  'meal': {
+    'total': {
+      'calories': 0,
+      'fat': 0,
+      'cholesterol': 0,
+      'sodium': 0,
+      'dietaryfiber': 0,
+      'sugar': 0,
+      'protein': 0,
+      'vitaminA': 0,
+      'vitaminB': 0,
+      'vitaminC': 0,
+      'vitaminD': 0,
+      'calcium': 0,
+      'iron': 0,
+      'potassium': 0
+    }
+  }
+}
+
+const lib = require('lib');
+const Clarifai = require('clarifai');
+var app = new Clarifai.App({
+  apiKey: 'c33b0471e6ab471e99aee1094594771e'
+});
+var request = require('request');
+var reqProm = require('request-promise');
+var TinyURL = require('tinyurl');
+// var {promisify} = require('util');
+// var shortener = promisify(TinyURL.shorten);
+
+function confirm(clarifaiArray, ibmArray) {
+  let confirmArray = [];
+  for (a = 0; a < clarifaiArray.length; a++) {
+    for (b = 0; b < ibmArray.length; b++) {
+      if (clarifaiArray[a] == ibmArray[b]) {
+        confirmArray.push(clarifaiArray[a]);
+      }
+    }
+  }
+  return confirmArray;
+}
+
+function addNutrients(item) {
+  meal.meal.total.calories += food.food[item].calories;
+  meal.meal.total.fat += food.food[item].fat;
+  meal.meal.total.cholesterol += food.food[item].cholesterol;
+  meal.meal.total.sodium += food.food[item].sodium;
+  meal.meal.total.dietaryfiber += food.food[item].dietaryfiber;
+  meal.meal.total.sugar += food.food[item].sugar;
+  meal.meal.total.protein += food.food[item].protein;
+  meal.meal.total.vitaminA += food.food[item].vitaminA;
+  meal.meal.total.vitaminB += food.food[item].vitaminB;
+  meal.meal.total.vitaminC += food.food[item].vitaminC;
+  meal.meal.total.vitaminD += food.food[item].vitaminD;
+  meal.meal.total.calcium += food.food[item].calcium;
+  meal.meal.total.iron += food.food[item].iron;
+  meal.meal.total.potassium += food.food[item].potassium;
+  meal.meal[item] = food.food[item];
+}
+
+function suggestions(nutrient) {
+  if (nutrient == 1)
+    return "Salmon, avacado, nuts, olives";
+  else if (nutrient == 2)
+    return "Eggs, sausages, cheese, milk";
+  else if (nutrient == 3)
+    return "Pickles, soya sauce, ham, celery";
+  else if (nutrient == 4)
+    return "Oatmeal, flax seeds, broccoli, lentils";
+  else if (nutrient == 6)
+    return "Lean beef, beans, soy products, fish";
+  else if (nutrient == 7)
+    return "Carrots, sweet potato, kale, spinach";
+  else if (nutrient == 8)
+    return "Seafood, banana, leafy greens, green peas";
+  else if (nutrient == 9)
+    return "Yellow bell peppers, guavas, oranges, broccoli";
+  else if (nutrient == 10)
+    return "Fatty fish, diary products, egg yolk, beef liver";
+  else if (nutrient == 11)
+    return "Milk, cheese, yogurt, kale";
+  else if (nutrient == 12)
+    return "Red meats, seafoods, beans, dark green leafy vegetables";
+  else if (nutrient = 13)
+    return "Banana, winter squash, potato, white beans";
+  else {
+    return "No suggestions ):"
+  }
+}
+
+function checkMin() {
+  let minPercent = parseInt(meal.meal.total.fat);
+  let minNum = 1;
+  if (parseInt(meal.meal.total.cholesterol) < minPercent){
+    minNum = 2;
+    minPercent = parseInt(meal.meal.total.cholesterol);
+  }
+  if (parseInt(meal.meal.total.sodium) < minPercent){
+        minNum = 3;
+        minPercent = parseInt(meal.meal.total.sodium);
+  }
+  if (parseInt(meal.meal.total.dietaryfiber) < minPercent) {
+    minNum = 4;
+    minPercent = parseInt(meal.meal.total.dietaryfiber);
+  }
+  if (parseInt(meal.meal.total.protein) < minPercent){
+    minNum = 6;
+    minPercent = parseInt(meal.meal.total.protein);
+  }
+  if (parseInt(meal.meal.total.vitaminA) < minPercent){
+    minNum = 7;
+    minPercent = parseInt(meal.meal.total.vitaminA);
+  }
+  if (parseInt(meal.meal.total.vitaminB) < minPercent){
+    minNum = 8;
+    minPercent = parseInt(meal.meal.total.vitaminB);
+  }
+  if (parseInt(meal.meal.total.vitaminC) < minPercent){
+    minNum = 9;
+    minPercent = parseInt(meal.meal.total.vitaminC);
+  }
+  if (parseInt(meal.meal.total.vitaminD) < minPercent) {
+    minNum = 10;
+    minPercent = parseInt(meal.meal.total.vitaminD);
+  }
+  if (parseInt(meal.meal.total.calcium) < minPercent) {
+    minNum = 11;
+    minPercent = parseInt(meal.meal.total.calcium);
+  }
+  if (parseInt(meal.meal.total.iron) < minPercent) {
+    minNum = 12;
+    minPercent = parseInt(meal.meal.total.iron);
+  }
+  if (parseInt(meal.meal.total.potassium) < minPercent) {
+    minNum = 13;
+    minPercent = parseInt(meal.meal.total.potassium);
+  }
+  return minNum;
+}
+
+function checkItem(index) {
+  if (index == 0)
+    return "calories";
+  else if (index == 1)
+    return "fat";
+  else if (index == 2)
+    return "cholesterol";
+  else if (index == 3)
+    return "sodium";
+  else if (index == 4)
+    return "dietaryfiber";
+  else if (index == 5)
+    return "sugar";
+  else if (index == 6)
+    return "protein";
+  else if (index == 7)
+    return "vitaminA";
+  else if (index == 8)
+    return "vitaminB";
+  else if (index == 9)
+    return "vitaminC";
+  else if (index == 10)
+    return "vitaminD";
+  else if (index == 11)
+    return "calcium";
+  else if (index == 12)
+    return "iron";
+  else if (index == 13)
+    return "potassium";
+}
+/**
+ * @param {string} URL Image of food
+ * @returns {any}
+ */
+module.exports = (URL = 'https://firebasestorage.googleapis.com/v0/b/fir-bd201.appspot.com/o/tmpDir%2FtmpImage.jpg?alt=media&token=5e5583a8-4633-48fe-ae2b-56f7ee2f4344', context, callback) => {
+  console.log(URL);
+  TinyURL.shorten(URL, function(res) {
+    URL = res;
+    console.log(URL)
+    //CLARIFAI CALL
+    app.models
+      .predict("bd367be194cf45149e75f01d59f77ba7", URL)
+      .then(response => {
+        const clarifaiArray = [];
+        for (i = 0; i < response.outputs[0].data.concepts.length; i++) {
+          if (response.outputs[0].data.concepts[i].value > 0.7) {
+            clarifaiArray.push(response.outputs[0].data.concepts[i].name);
+          }
+        }
+        console.log("clarifai done");
+        return clarifaiArray;
+      }).then(clarifaiArrayResult => {
+        console.log("ibm called")
+        console.log('url: ', URL);
+        let params = {
+          api_key: process.env.WATSON_KEY,
+          classfier_ids: "food",
+          version: "2016-05-20",
+          url: URL
+        }
+        console.log(params)
+        //const ibmURL = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifyapi_key=8d7aced8efa9ce11cca985d203dce5989cc20148&classifer_ids=food&url="+bunURL+"&version=2016-05-20"
+        // const ibmResponse = ibmCall(ibmURL);
+        //console.log('ibm url:  ', ibmURL);
+        return reqProm({
+            url: 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify',
+            qs: params
+          })
+          .then(function(ibmResponse) {
+            console.log('should have both')
+            return [clarifaiArrayResult, ibmResponse];
+          })
+      }).then(postIbmData => {
+        console.log("next");
+        const ibmArray = [];
+        const body = JSON.parse(postIbmData[1]);
+        let classifier = body.images[0].classifiers[0];
+        for (i = 0; i < classifier.classes.length; i++) {
+          if (classifier.classes[i].score > 0.5) {
+            ibmArray.push(classifier.classes[i].class);
+          }
+        }
+        return {
+          clarifaiArrayResult: postIbmData[0],
+          ibmResult: ibmArray
+        }
+      }).then(confirmResult => {
+        let theGoodFood = confirm(confirmResult.clarifaiArrayResult, confirmResult.ibmResult);
+        for (a = 0; a < theGoodFood.length; a++) {
+          let item = theGoodFood[a].replace(/['"]+/g, '');
+          if (food.food[item] !== undefined) {
+            addNutrients(item);
+          }
+        }
+        console.log("MAASD")
+        let minValueIndex = checkMin();
+        console.log("MAASD")
+        let suggestion = suggestions(minValueIndex);
+        console.log("Min value item")
+        let minValueItem = checkItem(minValueIndex);
+        console.log(minValueIndex)
+        console.log(minValueItem)
+        let low = {
+          'nutrient': minValueItem,
+          'sugestion': suggestion
+        };
+        meal.meal["suggest"] = low;
+        callback(null, meal);
+      })
+      .catch(err => {
+        console.log('error: ', err.data.outputs[0])
+        callback(err)
+      })
+  })
+
+};
